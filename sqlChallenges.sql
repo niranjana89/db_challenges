8274e154e0bb5887f19ae31d1c72bb55bf02b0d4
niranjana89 <30704985+niranjana89@users.noreply.github.com>
2021-08-01 10:46:47 +0530

    Add files via upload

---
+++ sqlChallenges.sql
@@ -0,0 +1,356 @@
+-- Question 21
+-- Table: ActorDirector
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | actor_id    | int     |
+-- | director_id | int     |
+-- | timestamp   | int     |
+-- +-------------+---------+
+-- timestamp is the primary key column for this table.
+ 
+
+-- Write a SQL query for a report that provides the pairs (actor_id, director_id) where the actor have cooperated with the director at least 3 times.
+
+-- Example:
+
+-- ActorDirector table:
+-- +-------------+-------------+-------------+
+-- | actor_id    | director_id | timestamp   |
+-- +-------------+-------------+-------------+
+-- | 1           | 1           | 0           |
+-- | 1           | 1           | 1           |
+-- | 1           | 1           | 2           |
+-- | 1           | 2           | 3           |
+-- | 1           | 2           | 4           |
+-- | 2           | 1           | 5           |
+-- | 2           | 1           | 6           |
+-- +-------------+-------------+-------------+
+
+-- Result table:
+-- +-------------+-------------+
+-- | actor_id    | director_id |
+-- +-------------+-------------+
+-- | 1           | 1           |
+-- +-------------+-------------+
+-- The only pair is (1, 1) where they cooperated exactly 3 times.
+SELECT actor_id, director_id FROM sys.actordirector group by actor_id, director_id having count(*)>=3;
+
+
+
+-- Question 13
+-- Table: Ads
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | ad_id         | int     |
+-- | user_id       | int     |
+-- | action        | enum    |
+-- +---------------+---------+
+-- (ad_id, user_id) is the primary key for this table.
+-- Each row of this table contains the ID of an Ad, the ID of a user and the action taken by this user regarding this Ad.
+-- The action column is an ENUM type of ('Clicked', 'Viewed', 'Ignored').
+ 
+
+-- A company is running Ads and wants to calculate the performance of each Ad.
+
+-- Performance of the Ad is measured using Click-Through Rate (CTR) where:
+
+
+
+-- Write an SQL query to find the ctr of each Ad.
+
+-- Round ctr to 2 decimal points. Order the result table by ctr in descending order and by ad_id in ascending order in case of a tie.
+
+-- The query result format is in the following example:
+
+-- Ads table:
+-- +-------+---------+---------+
+-- | ad_id | user_id | action  |
+-- +-------+---------+---------+
+-- | 1     | 1       | Clicked |
+-- | 2     | 2       | Clicked |
+-- | 3     | 3       | Viewed  |
+-- | 5     | 5       | Ignored |
+-- | 1     | 7       | Ignored |
+-- | 2     | 7       | Viewed  |
+-- | 3     | 5       | Clicked |
+-- | 1     | 4       | Viewed  |
+-- | 2     | 11      | Viewed  |
+-- | 1     | 2       | Clicked |
+-- +-------+---------+---------+
+-- Result table:
+-- +-------+-------+
+-- | ad_id | ctr   |
+-- +-------+-------+
+-- | 1     | 66.67 |
+-- | 3     | 50.00 |
+-- | 2     | 33.33 |
+-- | 5     | 0.00  |
+-- +-------+-------+
+-- for ad_id = 1, ctr = (2/(2+1)) * 100 = 66.67
+-- for ad_id = 2, ctr = (1/(1+2)) * 100 = 33.33
+-- for ad_id = 3, ctr = (1/(1+1)) * 100 = 50.00
+-- for ad_id = 5, ctr = 0.00, Note that ad_id = 5 has no clicks or views.
+-- Note that we don't care about Ignored Ads.
+-- Result table is ordered by the ctr. in case of a tie we order them by ad_id
+
+create temporary table t1 select ad_id, sum(case when action='Clicked' then 1 else 0 end) as total_clicks from ads  group by ad_id;
+
+create temporary table t2 select ad_id, sum(case when action != 'Ignored' then 1 else 0 end) as impr from ads  group by ad_id;
+
+select coalesce(t1.ad_id, t2.ad_id) as ad_id, coalesce(round(100*t1.total_clicks/t2.impr, 2), 0.0) as ctr from t1 join t2 where t1.ad_id = t2.ad_id order by ctr desc;
+
+-- Question 42
+-- Table: Views
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | article_id    | int     |
+-- | author_id     | int     |
+-- | viewer_id     | int     |
+-- | view_date     | date    |
+-- +---------------+---------+
+-- There is no primary key for this table, it may have duplicate rows.
+-- Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+-- Note that equal author_id and viewer_id indicate the same person.
+ 
+-- Write an SQL query to find all the authors that viewed at least one of their own articles, sorted in ascending order by their id.
+
+-- The query result format is in the following example:
+
+-- Views table:
+-- +------------+-----------+-----------+------------+
+-- | article_id | author_id | viewer_id | view_date  |
+-- +------------+-----------+-----------+------------+
+-- | 1          | 3         | 5         | 2019-08-01 |
+-- | 1          | 3         | 6         | 2019-08-02 |
+-- | 2          | 7         | 7         | 2019-08-01 |
+-- | 2          | 7         | 6         | 2019-08-02 |
+-- | 4          | 7         | 1         | 2019-07-22 |
+-- | 3          | 4         | 4         | 2019-07-21 |
+-- | 3          | 4         | 4         | 2019-07-21 |views
+-- +------------+-----------+-----------+------------+
+
+-- Result table:
+-- +------+
+-- | id   |
+-- +------+
+-- | 4    |
+-- | 7    |
+-- +------+
+
+select distinct author_id from views where author_id = viewer_id order by author_id asc;
+
+-- Question 39
+-- Table: Prices
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | product_id    | int     |
+-- | start_date    | date    |
+-- | end_date      | date    |
+-- | price         | int     |
+-- +---------------+---------+
+-- (product_id, start_date, end_date) is the primary key for this table.
+-- Each row of this table indicates the price of the product_id in the period from start_date to end_date.
+-- For each product_id there will be no two overlapping periods. That means there will be no two intersecting periods for the same product_id.
+ 
+
+-- Table: UnitsSold
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | product_id    | int     |
+-- | purchase_date | date    |
+-- | units         | int     |
+-- +---------------+---------+
+-- There is no primary key for this table, it may contain duplicates.
+-- Each row of this table indicates the date, units and product_id of each product sold. 
+ 
+
+-- Write an SQL query to find the average selling price for each product.
+
+-- average_price should be rounded to 2 decimal places.
+
+-- The query result format is in the following example:
+
+-- Prices table:
+-- +------------+------------+------------+--------+
+-- | product_id | start_date | end_date   | price  |
+-- +------------+------------+------------+--------+
+-- | 1          | 2019-02-17 | 2019-02-28 | 5      |
+-- | 1          | 2019-03-01 | 2019-03-22 | 20     |
+-- | 2          | 2019-02-01 | 2019-02-20 | 15     |
+-- | 2          | 2019-02-21 | 2019-03-31 | 30     |
+-- +------------+------------+------------+--------+
+ 
+-- UnitsSold table:
+-- +------------+---------------+-------+
+-- | product_id | purchase_date | units |
+-- +------------+---------------+-------+
+-- | 1          | 2019-02-25    | 100   |
+-- | 1          | 2019-03-01    | 15    |
+-- | 2          | 2019-02-10    | 200   |
+-- | 2          | 2019-03-22    | 30    |
+-- +------------+---------------+-------+
+
+-- Result table:
+-- +------------+---------------+
+-- | product_id | average_price |
+-- +------------+---------------+
+-- | 1          | 6.96          |
+-- | 2          | 16.96         |
+-- +------------+---------------+
+-- Average selling price = Total Price of Product / Number of products sold.
+-- Average selling price for product 1 = ((100 * 5) + (15 * 20)) / 115 = 6.96
+-- Average selling price for product 2 = ((200 * 15) + (30 * 30)) / 230 = 16.96
+
+
+
+
+select a.product_id, round(sum(a.price*b.units)/sum(b.units),2) from prices a join unitsSold b where a.product_id = b.product_id and b.purchase_date between a.start_date and  a.end_date group by a.product_id;
+
+
+-- Question 5
+-- There is a table World
+
+-- +-----------------+------------+------------+--------------+---------------+
+-- | name            | continent  | area       | population   | gdp           |
+-- +-----------------+------------+------------+--------------+---------------+
+-- | Afghanistan     | Asia       | 652230     | 25500100     | 20343000      |
+-- | Albania         | Europe     | 28748      | 2831741      | 12960000      |
+-- | Algeria         | Africa     | 2381741    | 37100000     | 188681000     |
+-- | Andorra         | Europe     | 468        | 78115        | 3712000       |
+-- | Angola          | Africa     | 1246700    | 20609294     | 100990000     |
+-- +-----------------+------------+------------+--------------+---------------+
+-- A country is big if it has an area of bigger than 3 million square km or a population of more than 25 million.
+
+-- Write a SQL solution to output big countries' name, population and area.
+
+-- For example, according to the above table, we should output:
+
+-- +--------------+-------------+--------------+
+-- | name         | population  | area         |
+-- +--------------+-------------+--------------+
+-- | Afghanistan  | 25500100    | 652230       |
+-- | Algeria      | 37100000    | 2381741      |
+-- +--------------+-------------+--------------+
+
+select name from world where area >= 3000000 or population >=25000000;
+
+-- Question 24
+-- Table my_numbers contains many numbers in column num including duplicated ones.
+-- Can you write a SQL query to find the biggest number, which only appears once.
+
+-- +---+
+-- |num|
+-- +---+
+-- | 8 |
+-- | 8 |
+-- | 3 |
+-- | 3 |
+-- | 1 |
+-- | 4 |
+-- | 5 |
+-- | 6 | 
+-- For the sample data above, your query should return the following result:
+-- +---+
+-- |num|
+-- +---+
+-- | 6 |
+-- Note:
+-- If there is no such number, just output null.
+
+select max(a.num) from (select num, count(num) from my_number group by num having count(num)=1)a;
+
+-- Question7
+-- There is a table courses with columns: student and class
+
+-- Please list out all classes which have more than or equal to 5 students.
+
+-- For example, the table:
+
+-- +---------+------------+
+-- | student | class      |
+-- +---------+------------+
+-- | A       | Math       |
+-- | B       | English    |
+-- | C       | Math       |
+-- | D       | Biology    |
+-- | E       | Math       |
+-- | F       | Computer   |
+-- | G       | Math       |
+-- | H       | Math       |
+-- | I       | Math       |
+-- +---------+------------+
+
+select class from courses group by class having count(distinct student)>=5;
+
+-- Question 14
+-- Table: Person
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | PersonId    | int     |
+-- | FirstName   | varchar |
+-- | LastName    | varchar |
+-- +-------------+---------+
+-- PersonId is the primary key column for this table.
+-- Table: Address
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | AddressId   | int     |
+-- | PersonId    | int     |
+-- | City        | varchar |
+-- | State       | varchar |
+-- +-------------+---------+
+-- AddressId is the primary key column for this table.
+ 
+
+-- Write a SQL query for a report that provides the following information for each person in the Person table,
+-- regardless if there is an address for each of those people:
+
+-- FirstName, LastName, City, State
+
+select person.firstName, person.lastName, address.city, address.state from person left join address on person.personId = address.personId;
+
+-- Question 37
+-- Several friends at a cinema ticket office would like to reserve consecutive available seats.
+-- Can you help to query all the consecutive available seats order by the seat_id using the following cinema table?
+-- | seat_id | free |
+-- |---------|------|
+-- | 1       | 1    |
+-- | 2       | 0    |
+-- | 3       | 1    |
+-- | 4       | 1    |
+-- | 5       | 1    |
+ 
+
+-- Your query should return the following result for the sample case above.
+ 
+
+-- | seat_id |
+-- |---------|
+-- | 3       |
+-- | 4       |
+-- | 5       |
+-- Note:
+-- The seat_id is an auto increment int, and free is bool ('1' means free, and '0' means occupied.).
+-- Consecutive available seats are more than 2(inclusive) seats consecutively available.
+
+select a.seat_id from (select seat_id, free, lag(free,1) over() as prev, lead(free, 1) over() as next from cinema)a where a.free=True and(a.next= True or a.prev=True);
+
+
+
+
+
+
